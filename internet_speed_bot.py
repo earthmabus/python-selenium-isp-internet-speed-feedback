91e1acf84e42723a19a901c0d89ec7c97fd3af56
Michael Popovich <earthmabus@hotmail.com>
2025-10-20 22:40:30 -0400

    updated bot to print results.

--- internet_speed_bot.py
+++ internet_speed_bot.py
@@ -3,8 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
-
-MAX_ELEMENT_WAIT_TIME_IN_SEC = 60
+import time
 
 class InternetSpeedBot:
     def __init__(self):
@@ -18,20 +17,22 @@ class InternetSpeedBot:
     def start(self):
         self.m_driver.get("https://www.speedtest.net")
 
-
     def get_internet_speed(self):
         '''Executes an internet speed test to track download and upload performance'''
+        time.sleep(2)
+
         elem_go_btn = self.m_driver.find_element(By.CLASS_NAME, "start-text")
         elem_go_btn.click()
 
-        # TODO start here -- this part does not work right now...
-        WebDriverWait(self.m_driver, MAX_ELEMENT_WAIT_TIME_IN_SEC).until(EC.presence_of_element_located((By.CSS_SELECTOR, "result-container-data")))
-        elem_label_download = WebDriverWait(self.m_driver, MAX_ELEMENT_WAIT_TIME_IN_SEC).until(EC.presence_of_element_located((By.CSS_SELECTOR, "data-upload-status-value")))
-        elem_label_upload = WebDriverWait(self.m_driver, MAX_ELEMENT_WAIT_TIME_IN_SEC).until(EC.presence_of_element_located((By.CSS_SELECTOR, "data-download-status-value")))
-        print(f"results: download={elem_label_download.text} Mbps, upload={elem_label_upload.text} Mbps")
-        # attribute data-upload-status-value="0.51"
-        # attribute data-download-status-value="0.35"
-        pass
+        time.sleep(60)
+
+        elem_label_download = self.m_driver.find_element(By.CLASS_NAME, "download-speed")
+        elem_label_upload = self.m_driver.find_element(By.CLASS_NAME, "upload-speed")
+        elem_label_ping_in_ms = self.m_driver.find_element(By.CLASS_NAME, "ping-speed")
+        elem_label_latency_up = self.m_driver.find_element(By.CSS_SELECTOR, '[data-latencyup-status-value]')
+        elem_label_latency_down = self.m_driver.find_element(By.CSS_SELECTOR, '[data-latencydown-status-value]')
+        #data-latency-up-status-value
+        print(f"results: download={elem_label_download.text} Mbps, upload={elem_label_upload.text} Mbps, ping={elem_label_ping_in_ms.text}, down={elem_label_latency_down.text}, up={elem_label_latency_up.text}")
 
     def store_results_into_sheet(self):
         '''Stores the results of a speed test in a google sheet'''
